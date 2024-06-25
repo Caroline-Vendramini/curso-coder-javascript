624f6da75f007bce92068bcdab5196a29fac737b
Caroline Vendramini <carolinevendramini@Air-de-Caroline.home>
2024-06-25 18:33:33 -0300

    Aulas this e bind, arrow function, funcoes anonimas

---
+++ funcao/callback1.js
@@ -0,0 +1,8 @@
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+function imprimir(nome, indice) {
+    console.log
+}
+
+fabricantes.forEach(imprimir)
+fabricantes.forEach(fabricante => console.log(fabricante))
